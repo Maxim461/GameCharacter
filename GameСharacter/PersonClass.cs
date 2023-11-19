febfca5b557d6030a859c2501413ab78d2956432
Maxim461 <Max2004lee@mail.ru>
2023-11-19 14:50:58 +0300

    init 2

---
+++ GameСharacter/PersonClass.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Xml.Linq;
+using Microsoft.VisualBasic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GameСharacter
+{
+	public class PersonClass
+    {
+        private string name;
+        private string camp;
+        private int numberOfLives;
+        private int x;
+        private int y;
+
+		public PersonClass(string name, string camp, int numberOfLives, int x, int y)
+		{
+            this.name = name;
+            this.camp = camp;
+            this.numberOfLives = numberOfLives;
+            this.x = x;
+            this.y = y;
+		}
+
+        public string getCapm()
+        {
+            return camp;
+        }
+
+        public void InformationOutput()
+        {
+            Console.WriteLine($"Имя: {name}. Количество его жизней: {numberOfLives}. Координаты x:{x} y:{y}. Принадлежность к лагерю: {camp}.");
+        }
+
+        public void FullInformationOutput()
+		{
+            Console.WriteLine($"Имя: {name}. Количество его жизней: {numberOfLives}. Координаты x:{x} y:{y}. Принадлежность к лагерю: {camp}.");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------------");
+        }
+
+        public void Moving()
+        {
+            Console.Write("Введите движение по X: ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("Введите движение по Y: ");
+            y = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if ((x<=50&&x>=-50)&&(y <= 50 && y >= -50))
+                {
+                    break;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Координаты не могут быть больше 50 или меньше -50.");
+                    Console.ResetColor();
+                    Console.Write("Введите движение по X: ");
+                    x = int.Parse(Console.ReadLine());
+                    Console.Write("Введите движение по Y: ");
+                    y = int.Parse(Console.ReadLine());
+                    
+                }
+            }
+        }
+
+        public void FullDestruction()
+        {
+            numberOfLives = 0;
+            Console.WriteLine("Враг уничтожен.");
+        }
+
+        public void DealingDamage()
+        {
+            Random random = new Random();
+            int damage = random.Next(0, 3);
+            int damageEnemy = random.Next(0, 3);
+            numberOfLives = numberOfLives - damage;
+            Console.WriteLine($"Вы нанесли противнику: {damageEnemy}, осталось: {numberOfLives} единиц здоровья;\nОн вам нанес: {damage}, осталось: {numberOfLives} единиц здоровья.");
+        }
+
+        public void FullTreatment()
+        {
+            numberOfLives = 10;
+            Console.WriteLine($"Вы полностью восстановились, количество здоровья: {numberOfLives}.");
+        }
+
+        public void Treatment()
+        {
+            Console.Write("Введите количество жизней на которое вы хотите пополнить:");
+            int buttonTreatment =+ int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (buttonTreatment > 10 || buttonTreatment < 0)
+                {
+                    Console.WriteLine("Вы не можете иметь больше 10 единиц здоровья или меньше 1.");
+                    Console.Write("Введите количество жизней на которое вы хотите пополнить:");
+                    buttonTreatment =+ int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+    }
+}
--- GameСharacter/Program.cs
+++ GameСharacter/Program.cs
@@ -1,65 +1,206 @@
-﻿using GameСharacter;
+﻿using System;
+using System.Numerics;
+using GameСharacter;
+using System.Threading.Channels;
+using System.Collections.Generic;
+using System.Drawing;
 
 class Program
 {
     static void Main()
     {
-        GameChacterClass GameCharacter = new GameChacterClass("Человек", true, 10, 0, 0);
-
+        List<PersonClass> persons = new List<PersonClass>();
+        //Создание персонажей
+        Console.Write("Введите количество персонажей: ");
+        int numberOfPersons = int.Parse(Console.ReadLine());
         while (true)
         {
-            Console.WriteLine("Нажмите:");
-            Console.WriteLine("1 для вывода информации;");
-            Console.WriteLine("2 для перемещения по горизонтали;");
-            Console.WriteLine("3 для перемещения по вертикали;");
-            Console.WriteLine("4 для полного уничтожения;");
-            Console.WriteLine("5 для нанесения урона;");
-            Console.WriteLine("6 для лечения;");
-            Console.WriteLine("7 для полноного восстановления;");
-            Console.WriteLine("8 для принадлежности лагерю;");
-            Console.WriteLine("9 для смены лагеря.");
-
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-
-            switch (keyInfo.Key)
+            if (numberOfPersons <= 1)
             {
-                case ConsoleKey.D1:
-                    GameCharacter.InformationOutput();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Количество персонажей не может быть меньше или ровно одного.");
+                Console.ResetColor();
+                Console.Write("Введите количество персонажей: ");
+                numberOfPersons = int.Parse(Console.ReadLine());
+            }
+            else
+            {
+                break;
+            }
+        }
+        //Создание рандомов для персонажей и присвоение
+        for (int i = 0; i < numberOfPersons; i++)
+        {
+            Console.Write("Введите количество жизней: ");
+            int numberOfLives = int.Parse(Console.ReadLine());
+            Console.Write("Введите координату X: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите координату Y: ");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write("Введите имя персонажа: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите название лагеря: ");
+            string camp = Console.ReadLine();
+            persons.Add(new PersonClass(name, camp, numberOfLives, x, y));
+        }
+        //Вывод персонажей
+        Console.Clear();
+        for (int i = 0; i < numberOfPersons; i++)
+        {
+            Console.Write(i + 1 + " ");
+            persons[i].FullInformationOutput();
+        }
+        //Выбор персонажа
+        Console.Write("Введите цифру под каким вы персонажем хотите играть: ");
+        int hero = int.Parse(Console.ReadLine()) - 1;
+        while (true)
+        {
+            if (hero < 0 || hero >= numberOfPersons)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Вы можете выбрать персонажа только из того количетсва которое вы создали.");
+                Console.ResetColor();
+                Console.Write("Введите цифру под каким вы персонажем хотите играть: ");
+                hero = int.Parse(Console.ReadLine()) - 1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        while (true)
+        {   //Выбор действия для персонажа
+            Console.WriteLine("Нажмите:\n0 - если хотите вывести информацию о себе;\n1 - если хотите вывести информацию об игроках;\n2 - если хотите выполнить движение по X и Y;\n3 - если хотите вступить в бой;\n4 - если хотите полностью вылечиться;\n5 - если хотите подлечиться;\n6 - если хотите сменить персонажа.");
+            int button = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                if (button <= -1 || button >= 7)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Вы можете выбрать действия которого нету в списке.");
+                    Console.ResetColor();
+                    Console.WriteLine("Нажмите:\n0 - если хотите вывести информацию о себе;\n1 - если хотите вывести информацию об игроках;\n2 - если хотите выполнить движение по X и Y;\n3 - если хотите вступить в бой;\n4 - если хотите полностью вылечиться;\n5 - если хотите подлечиться;\n6 - если хотите сменить персонажа.");
+                    button = int.Parse(Console.ReadLine());
+                }
+                else
+                {
                     break;
-
-                case ConsoleKey.D2:
-                    GameCharacter.MovingHorizontally();
-                    break;
-
-                case ConsoleKey.D3:
-                    GameCharacter.MovingVertically();
-                    break;
-
-                case ConsoleKey.D4:
-                    GameCharacter.Destruction();
-                    break;
-
-                case ConsoleKey.D5:
-                    GameCharacter.DealingDamage();
-                    break;
-
-                case ConsoleKey.D6:
-                    GameCharacter.Treatment();
-                    break;
-
-                case ConsoleKey.D7:
-                    GameCharacter.FullRecovery();
-                    break;
-
-                case ConsoleKey.D8:
-                    GameCharacter.BelongingToTheCamp();
-                    break;
-
-                case ConsoleKey.D9:
-                    GameCharacter.CharacterChange();
-                    break;
-
-                default: break;
+                }
+            }
+            //Действия для персонажа
+            switch(button)
+            {   //Вывод информации о себе
+                case 0:
+                    Console.Clear();
+                    persons[hero].InformationOutput();
+                break;
+                //Вывод информации об игроках
+                case 1:
+                    Console.Clear();
+                    for (int i = 0; i < numberOfPersons; i++)
+                    {
+                        Console.Write(i  + 1 + " ");
+                        persons[i].FullInformationOutput();
+                    }
+                break;
+                //Движение по X и по Y
+                case 2:
+                    Console.Clear();
+                    persons[hero].Moving();
+                break;
+                //Бой с выбором полное уничтожение или нанесение урона
+                case 3:
+                    Console.Clear();
+                    //Выбор противника
+                    Console.Write("Введите цифру какого противника вы хотите атаковать: ");
+                    int opponent = int.Parse(Console.ReadLine()) - 1;
+                    while (true)
+                    {
+                        if (opponent < 0 || opponent >= numberOfPersons)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Вы можете выбрать персонажа только из того количетсва которое вы создали.");
+                            Console.ResetColor();
+                            Console.Write("Введите цифру какого противника вы хотите атаковать: ");
+                            opponent = int.Parse(Console.ReadLine()) - 1;
+                        }
+                        else
+                        {
+                            if (persons[hero].getCapm() != persons[opponent].getCapm())
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Вы не можете атаковать своих.");
+                                Console.ResetColor();
+                            }
+                        }
+                    }
+                    //Выбор боя: полное уничтожение или нанесение урона
+                    Console.WriteLine("Нажмите:\n1 - если вы хотите выполнить полное уничтожение;\n2 - если вы хотите нанести урон.");
+                    button = int.Parse(Console.ReadLine());
+                    while (true)
+                    {
+                        if (button == 1)
+                        {
+                            persons[opponent].FullDestruction();
+                            break;
+                        }
+                        else if (button == 2)
+                        {
+                            persons[opponent].DealingDamage();
+                            persons[hero].DealingDamage();
+                            break;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Ошибка, вы ввели не правильные данные.");
+                            Console.ResetColor();
+                            Console.WriteLine("Нажмите:\n1 - если вы хотите выполнить полное уничтожение;\n2 - если вы хотите нанести урон.");
+                            button = int.Parse(Console.ReadLine());
+                        }
+                    }
+                    
+                break;
+                //Полное восстановление здоровья
+                case 4:
+                    Console.Clear();
+                    persons[hero].FullTreatment();
+                break;
+                //Восстановление здоровья с выбором количетсва 
+                case 5:
+                    Console.Clear();
+                    persons[hero].Treatment();
+                break;
+                //Смена персонажа
+                case 6:
+                    Console.Clear();
+                    for (int i = 0; i < numberOfPersons; i++)
+                    {
+                        Console.Write(i + 1 + " ");
+                        persons[i].FullInformationOutput();
+                    }
+                    Console.Write("Введите цифру под каким вы персонажем хотите играть: ");
+                    hero = int.Parse(Console.ReadLine()) - 1;
+                    while(true)
+                    {
+                        if (hero < 0 || hero >= numberOfPersons)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Вы можете выбрать персонажа только из того количетсва которое вы создали.");
+                            Console.ResetColor();
+                            Console.Write("Введите цифру под каким вы персонажем хотите играть: ");
+                            hero = int.Parse(Console.ReadLine()) - 1;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                break;
             }
         }
     }
